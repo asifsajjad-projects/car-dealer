75f83277b1cab5ace9e1ef5d4d78212e498fc3bd
asifsajjad-projects <asifrps17@gmail.com>
2022-03-09 14:29:38 +0530

    added problem2.js to get last car.Exports: getLastCar()

---
+++ problem2.js
@@ -0,0 +1,12 @@
+const inventory= require('./car_inventory');
+
+// get last object from the array
+const last_car= inventory[inventory.length-1];
+
+function getLastCar()
+{
+    return `Last car is a ${last_car.car_make} ${last_car.car_model}.`;
+}
+console.log(getLastCar());
+
+module.exports = getLastCar;
