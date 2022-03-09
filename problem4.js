f4bf262c5aa4fef69a8f4f18e5eeceef065ee846
asifsajjad-projects <asifrps17@gmail.com>
2022-03-09 15:50:17 +0530

    Created problem4.js for car Years. Exports: carYear and uniqCarYears

---
+++ problem4.js
@@ -0,0 +1,36 @@
+const inventory = require('./car_inventory');
+
+const carYears= [];
+// add car years to the array
+for(let i=0; i< inventory.length;i++)
+{
+    carYears[i]=inventory[i].car_year;
+}
+
+const uniqCarYears =[];
+
+// remove the repeated car years
+for(let j=0; j<carYears.length;j++)
+{
+    let key=0;
+    year=carYears[j];
+    for(let k=0;k<uniqCarYears.length;k++)
+    {
+        if(year===uniqCarYears[k])
+        {
+            key=1;
+            continue;
+        }
+    }
+    if(key===0)
+    {
+        uniqCarYears.push(year);
+    }
+    
+}
+
+// console.log(carYears);
+// console.log(uniqCarYears);
+
+module.exports = carYears;
+module.exports = uniqCarYears;
